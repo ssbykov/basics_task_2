495433aaac440aa632862d82c372038766816c97
Sergei Bykov <ss.bykoff@gmail.com>
2023-10-31 18:01:06 +0300

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    for (i in (0..121))
+        outLikes(i)
+}
+
+fun outLikes(likes: Int) {
+    val people = if ((likes + 100) % 100 != 11 && (likes + 10) % 10 == 1) "человеку" else "людям"
+    println("Понравилось $likes $people")
+}
